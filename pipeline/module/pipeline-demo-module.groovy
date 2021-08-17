e21e45b6120d77bfe61cbde3c9d4b3bf83a7096b
rylgm <871547264@qq.com>
2021-08-17 23:30:57 +0800

    新增测试数据和demo测试

---
+++ pipeline/module/pipeline-demo-module.groovy
@@ -0,0 +1,26 @@
+import hudson.model.*;
+import groovy.json.*;
+
+def find_files(filetype) {
+	
+	def files = findFiles(glob:filetype)
+	for (file in files) {
+		println file.name
+	}
+}
+
+def read_json_file(file_path) {
+	def propMap = readJSON file : file_path
+	propMap.each {
+	    println ( it.key + " = " + it.value )
+	}
+}
+
+def read_json_file2(json_string) {
+	def propMap = readJSON text : json_string
+	propMap.each {
+		println ( it.key + " = " + it.value )
+	}
+}
+
+return this;
---
+++ pipeline/scripts/pipeline-utility-steps-test.groovy
@@ -0,0 +1,29 @@
+import hudson.model.*
+import sun.util.resources.cldr.nyn.CalendarData_nyn_UG;
+
+println env.JOB_NAME
+println env.BUILD_NUMBER
+
+pipeline{
+	agent any
+	stages{
+		stage('init'){
+			steps{
+				script{
+					model_test = load env.WORKSPACE + "/pipeline/module/pipeline-demo-module.groovy"
+				}
+			}
+		}
+		stages("Parse json"){
+			steps{
+				script{
+					json_file = env.WORKSPACE + "/testdata/test_json.json"
+					model_test.read_json_file(json_file)
+					println "==============================="
+					json_string = '{"NAME":"Tom","AGE":18,"CITY":"Bejjing"}'
+					model_test.read_json_file2(json_string)
+				}
+			}
+		}
+	}
+}
